b207f68b583f7cde965cceefc5024b3f380d7eaa
Sam-E18 <samuel.escu18@gmail.com>
2024-10-23 02:03:36 +0200

    a little change

--- peek.sh
+++ peek.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-#Print the first three and the las three lines of an input file
+#Print the first and the last lines of an input file, with 3 dots in the middle of it.
+#(You need to put the numeber of lines after the file).
+#Ejm. bash peek.sh tables/target_prokaryotes.txt 3
 
-head -n 3 "$1"
+head -n "$2" "$1"
 echo "..."
-tail -n 3 "$1"
+tail -n "$2" "$1"
+
